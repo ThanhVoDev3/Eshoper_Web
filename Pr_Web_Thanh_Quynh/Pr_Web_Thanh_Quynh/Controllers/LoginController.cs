a5ee881b8555b5fa59cd0b8d6660b6e8a89d5596
unknown <Vanthanh@gmail.com>
2024-03-16 22:44:29 +0700

    add login

--- Pr_Web_Thanh_Quynh/Pr_Web_Thanh_Quynh/Controllers/LoginController.cs
+++ Pr_Web_Thanh_Quynh/Pr_Web_Thanh_Quynh/Controllers/LoginController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Pr_Web_Thanh_Quynh.Areas.Admin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,8 +10,19 @@ namespace Pr_Web_Thanh_Quynh.Controllers
     public class LoginController : Controller
     {
         // GET: Login
+        [HttpGet]
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index (string Acc , string Pass) 
+        {
+            bool isAuthentic = (Acc != null && Pass != null && Acc.ToLower().Equals("admin") && Pass.Equals("123456"));
+            if (isAuthentic)
+                return View ("~/Areas/Admin/Views/Dashboard/Index.cshtml");
             return View();
         }
     }
